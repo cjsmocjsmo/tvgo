fbfa0a9863eda30c636a7f439507d6ca8e946ad1
Charlie <porthose.cjsmo@gmail.com>
2020-09-20 06:26:38 -0700

    corrected paths

--- tvshows.go
+++ tvshows.go
@@ -185,9 +185,9 @@ func getTvShowInfo(apath string, tvshowpicPath string) (TvSI TVShowInfoS) {
 			TvSI.TVShowPicPath = tvshowpicPath
 			TvSI.TvFSPath = fspath
 			TvSI.Catagory = "Picard"
-			TvSI.Season = filename[18:20]
-			TvSI.Episode = filename[21:23]
-			TvSI.Title = filename[23:boo]
+			TvSI.Season = filename[19:21]
+			TvSI.Episode = filename[22:24]
+			TvSI.Title = filename[24:boo]
 			TvSI.Series = "Picard"
 	}
 	fmt.Printf("\n THIS IS TVI FROM TVSHOWS \n %s \n", TvSI)
