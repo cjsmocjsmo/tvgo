eb93d8503bfb299905904c3b6bee7fecdfbe2889
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-03-26 09:44:50 -0700

    added for all man kind

--- tvshows.go
+++ tvshows.go
@@ -253,6 +253,26 @@ func getTvShowInfo(apath string, tvshowpicPath string) (TvSI TVShowInfoS) {
 		TvSI.Title = filename[23:boo]
 		TvSI.Series = "RaisedByWolves"
 
+
+	case strings.Contains(apath, "For All Man Kind"):
+		_, filename := path.Split(apath)
+		fspath := apath[20:]
+		boo := len(filename) - 4
+		TvSI.ID = bson.NewObjectId()
+		TvSI.FilePath = apath
+		TvSI.MediaID = tvshowsUUID()
+		TvSI.Genre = "TVShows"
+		TvSI.TVShowPicPath = tvshowpicPath
+		TvSI.TvFSPath = fspath
+		TvSI.Catagory = "ForAllManKind"
+		TvSI.Season = filename[17:19]
+		TvSI.Episode = filename[20:22]
+		TvSI.Title = filename[22:boo]
+		TvSI.Series = "ForAllManKind"
+
+
+
+
 	case strings.Contains(apath, "SpaceTime"):
 		_, filename := path.Split(apath)
 		fspath := apath[20:]
